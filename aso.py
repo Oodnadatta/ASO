0f9c9bd88942df15817f9f7c2d29d0cc28b541a3
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2023-12-03 16:48:17 +0100

    Count missense and premature stop codon variants in ClinVar

--- aso.py
+++ aso.py
@@ -4,6 +4,7 @@
 # asdeno@hotmail.fr
 
 import csv
+import pprint
 import sys
 
 def get_green_from_panelapp_file(file_path):
@@ -19,7 +20,7 @@ def enumerate_clinvar_data(file_path):
     with open(file_path) as clinvar_file:
         for line in clinvar_file:
             if not line.startswith('#'):
-                info = line.split('\t')[7]
+                info = line.rstrip().split('\t')[7]
                 row_dict = {}
                 for key_value in info.split(';'):
                     key, value = key_value.split('=')
@@ -38,6 +39,9 @@ def enumerate_clinvar_data(file_path):
             
 if __name__ == "__main__":
     green_genes_dict = get_green_from_panelapp_file(sys.argv[1])
+    for green_gene in green_genes_dict.values():
+        green_gene['missense_count'] = 0 # Add 'missense_count' in green_genes_dict
+        green_gene['premature_stop_codon_count'] = 0
     for clinvar_row in enumerate_clinvar_data(sys.argv[2]):
         if 'GENEINFO' in clinvar_row:
             for gene, omim in clinvar_row['GENEINFO']:
@@ -47,5 +51,33 @@ if __name__ == "__main__":
                         for molecular_consequence in clinvar_row['MC']:
                             # clinvar_row['MC'] : value from 'MC' = [['SO:0001624', '3_prime_UTR_variant']]
                             variant_type = molecular_consequence[1]
-                            variant_types.append(variant_type)
-                    # print(clinvar_row['CLNSIG'], gene, variant_types) = 5 Uncertain_significance GNB1 ['3_prime_UTR_variant']
+                            #variant_types.append(variant_type)
+                        # print(clinvar_row['CLNSIG'], gene, variant_types) = 5 Uncertain_significance GNB1 ['3_prime_UTR_variant']
+                            if variant_type in ['inframe_deletion',
+                                                'inframe_indel',
+                                                'inframe_insertion',
+                                                'missense_variant']:
+                                green_genes_dict[gene]['missense_count'] += 1
+                            elif variant_type in ['frameshift_variant',
+                                                  'nonsense',
+                                                  'splice_acceptor_variant',
+                                                  'splice_donor_variant']:
+                                green_genes_dict[gene]['premature_stop_codon_count'] += 1
+                            elif variant_type in ['3_prime_UTR_variant',
+                                                  '5_prime_UTR_variant',
+                                                  'genic_downstream_transcript_variant',
+                                                  'genic_upstream_transcript_variant',
+                                                  'initiator_codon_variant',
+                                                  'intron_variant',
+                                                  'no_sequence_alteration',
+                                                  'non-coding_transcript_variant',
+                                                  'stop_lost',
+                                                  'synonymous_variant']:
+                                pass
+                            else:
+                                print(f'Unknown variant_type: {variant_type}', file=sys.stderr)
+                                sys.exit(1)
+
+    pprint.pprint(green_genes_dict)
+                                
+                
--- launcher_example.sh
+++ launcher_example.sh
@@ -5,3 +5,4 @@
 
 ./aso.py "./data/Early onset or syndromic epilepsy.tsv" "./data/clinvar_20231126.vcf" | head -n 500
 
+
