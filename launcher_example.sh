2b19c030ea3cf928849fd753e5f2ea70dfb46db3
Anne-Sophie Denommé-Pichon <asdeno@hotmail.fr>
2024-03-16 22:14:56 +0100

    Update ClinVar data

--- launcher_example.sh
+++ launcher_example.sh
@@ -5,7 +5,7 @@
 
 ./aso.py "./data/Early onset or syndromic epilepsy.tsv" \
 	 "./data/2023-12-09_genemap2.txt" \
-	 "./data/clinvar_20231126.vcf" \
+	 "./data/clinvar_20240312.vcf" \
 	 "./data/gnomad.v4.0.constraint_metrics.tsv" \
 	 "./data/gnomad.v2.1.1.lof_metrics.by_gene.txt" \
 	 "./data/MANE.tsv" \
